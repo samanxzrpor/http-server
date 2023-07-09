589a415b01f2c94fdeb4be2ac5c1b0bca6845027
samanxzrpor <xzrpor@gmail.com>
2023-07-09 19:54:30 +0330

    create a cmd madule for server side

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/samanxzrpor/http-server/pkg/config"
+	"github.com/samanxzrpor/http-server/pkg/server"
+)
+
+func main() {
+
+	config := config.LoadConfigOrPanic()
+	server := server.NewHttpServer(config)
+	server.HandlerFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	server.Start()
+}
