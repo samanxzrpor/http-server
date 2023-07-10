01ea5ba05ff74a13292346a445d94a1ac80f658a
samanxzrpor <xzrpor@gmail.com>
2023-07-10 20:28:49 +0330

    Add database config struct to config pkg

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,19 +3,29 @@ package config
 import "github.com/caarlos0/env/v9"
 
 type Server struct {
-	Host string `env:"HOST"`
-	Port int    `env:"PORT"`
+	Host string `env:"SERVERHOST"`
+	Port int    `env:"SERVERPORT"`
 }
+
+type Database struct {
+	User string `env:"DBUSER"`
+	Pass string `env:"DBPASS"`
+	Host string `env:"DBHOST"`
+	Port int    `env:"DBPORT"`
+	Db   string `env:"DBDB"`
+}
+
 type Config struct {
 	Server
+	Database
 }
 
 func LoadConfigOrPanic() Config {
-	var config *Config = new(Config)
 
-	if err := env.Parse(config); err != nil {
+	var cfg *Config = new(Config)
+	if err := env.Parse(cfg); err != nil {
 		panic(err)
 	}
 
-	return *config
+	return *cfg
 }
